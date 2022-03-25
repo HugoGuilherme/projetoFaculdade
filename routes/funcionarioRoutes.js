8bbefdb904d2a09ea5adad1cc1adccaf0007f3d2
wandbergdelima <wandberg@gmail.com>
2022-03-25 17:06:57 -0300

    danger

--- routes/funcionarioRoutes.js
+++ routes/funcionarioRoutes.js
@@ -6,7 +6,7 @@ const UserController = require('../controllers/UserController')
 //helpers
 const checkAuth = require('../helpers/auth').checkAuth
 
-router.get('/dashboard', checkAuth, function (req, res) {
+router.get('/dashboard', function (req, res) {
     res.render('areaFuncionario/funcionarioHome')
 })
 
@@ -24,19 +24,19 @@ router.get('/dashboard/relatorios', checkAuth, function (req, res) {
 
 //Perfil funcionario
 router.get('/dashboard/perfil', checkAuth, funcionarioController.atualizaFuncionarioPerfil)
-router.post('/dashboard/atualizaFuncionarioPerfilPost',  funcionarioController.atualizaFuncionarioPerfilPost)
+router.post('/dashboard/atualizaFuncionarioPerfilPost', funcionarioController.atualizaFuncionarioPerfilPost)
 
 //Clientes cadastrados
 router.get('/dashboard/clientesCadastrados', checkAuth, funcionarioController.clientesCadastrados)
-router.post('/dashboard/removeCliente/:id', checkAuth, funcionarioController.deletaCliente) 
+router.post('/dashboard/removeCliente/:id', checkAuth, funcionarioController.deletaCliente)
 router.get('/dashboard/edit/:id', checkAuth, funcionarioController.atualizaCliente)
 router.post('/dashboard/updateCliente', checkAuth, funcionarioController.atualizaClientetPost)
 
 //Funcionarios cadastrados
 router.get('/dashboard/funcionariosCadastrados', checkAuth, funcionarioController.funcionariosCadastrados)
-router.post('/cadastrarUsuario', checkAuth, UserController.cadastrarUsuario)
+router.post('/cadastrarUsuario', UserController.cadastrarUsuario)
 router.get('/dashboard/editFuncionario/:id', checkAuth, funcionarioController.atualizaFuncionario)
 router.post('/dashboard/updateFuncionario', checkAuth, funcionarioController.atualizaFuncionariotPost)
-router.post('/dashboard/removeFuncionario/:id', checkAuth, funcionarioController.deletaFuncionario) 
+router.post('/dashboard/removeFuncionario/:id', checkAuth, funcionarioController.deletaFuncionario)
 
-module.exports= router
+module.exports = router
